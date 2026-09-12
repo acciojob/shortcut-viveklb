65bdb4e4266ca69a24e7bdfa3c6ca1c767c7c69b
vivek bharamshetti <69708672+viveklb@users.noreply.github.com>
2026-09-12 12:59:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function shortcut(s1, s2) {
-  // your code here
+  // Return an empty string if either of the strings is empty
+  if (s1 === "" || s2 === "") {
+    return "";
+  }
+  
+  // Return the first character of both strings concatenated
+  return s1.charAt(0) + s2.charAt(0);
 }
 
 // Do not change the code below.
